06ea34e7a20ffe38b8d6f642fd61f1810b4247b2
PabloRodrz <pablorodrz09@gmail.com>
2023-02-01 18:31:37 -0300

    filter and styling done. To fix: mobile responsiveness

--- src/components/sandwiches/Sandwiches.tsx
+++ src/components/sandwiches/Sandwiches.tsx
@@ -2,32 +2,56 @@ import './sandwiches.css'
 import { sandwiches } from '../../assets/data.json'
 import { useState } from 'react'
 const Sandwiches = () => {
+  interface Sandwich {
+    name: string;
+    ingredient: string[];
+    seasoning: string[];
+    "meal-powers": string[];
+  }
+
   //selectores: exp no tiene tipo. Los demás tienen tipo y meal power
   const [type, setType] = useState("")
-  const [mealPower, setMealPower] = useState("")
+  const [userMealPower, setUserMealPower] = useState("")
   const handleType = (e: any) => {
     setType(e.target.value)
   }
   const handleMealPower = (e: any) => {
-    setMealPower(e.target.value)
+    setUserMealPower(e.target.value)
   }
-
-  const filteredSandwiches = sandwiches.filter(sandwich => {
-    if (type.length > 0) {
-     console.log(sandwich['meal-powers'].forEach((mealPower) => {
+  const filteredSandwiches: Sandwich[] = []
+  const sandwichFilter = sandwiches.filter(sandwich => {
+    if (type.length > 0 && userMealPower.length > 0) {
+      sandwich['meal-powers'].filter((mealPower) => {
         if (mealPower.split(" ").includes(type)) {
-          return sandwich
+          if (mealPower.split(" ").includes(userMealPower)) {
+            filteredSandwiches.push(sandwich)
+          }
         }
-      }))
+      })
+      return
     }
+    if (type.length > 0) {
+      sandwich['meal-powers'].filter((mealPower) => {
+        if (mealPower.split(" ").includes(type)) {
+          filteredSandwiches.push(sandwich)
+        }
+      })
+    }
+    if (userMealPower.length > 0) {
+      sandwich['meal-powers'].filter((mealPower) => {
+        if (mealPower.split(" ").includes(userMealPower)) {
+          filteredSandwiches.push(sandwich)
+        }
+      })
+    }
+
   })
-  console.log(filteredSandwiches)
   return (
     <div className="sandwich-con container-fluid d-flex justify-content-center col-md-10 col-sm-12">
       <div className='table-responsive-md'>
         <div className='pt-5 d-flex justify-content-end'>
           <div>
-            <select name="type" onChange={handleType} className='rounded border border-none'>
+            <select name="type" onChange={handleType} className='rounded'>
               <option value="" hidden>Choose the pokemon type</option>
               <option value="Normal">Normal</option>
               <option value="Fire">Fire</option>
@@ -50,7 +74,7 @@ const Sandwiches = () => {
             </select>
           </div>
           <div className='mp-select' onChange={handleMealPower}>
-            <select name="meal-power" className='rounded border border-none'>
+            <select name="meal-power" className='rounded'>
               <option value="" hidden>Choose a meal power</option>
               <option value="Encounter">Encounter</option>
               <option value="Title">Title</option>
@@ -65,31 +89,46 @@ const Sandwiches = () => {
             </select>
           </div>
         </div>
-        <table className="table table-bordered table-hover mt-5">
+        <table className="table table-borderless table-hover mt-5">
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Ingredient(s)</th>
-              <th>Seasoning</th>
-              <th>Meal power</th>
+              <th className='px-4'>Name</th>
+              <th className='px-4'>Ingredient(s)</th>
+              <th >Seasoning</th>
+              <th >Meal power</th>
             </tr>
           </thead>
           <tbody>
-            {filteredSandwiches.map((sandwich, index) => {
+            {filteredSandwiches.length > 0 && filteredSandwiches.map((sandwich, index) => {
               return <tr key={index}>
-                <td>{sandwich.name}</td>
-                <td>{sandwich.ingredient.map((ingredient, idx) => {
+                <td className='px-4'>{sandwich.name}</td>
+                <td className='px-4'>{sandwich.ingredient.map((ingredient, idx) => {
                   return sandwich.ingredient.length - 1 === idx ? ingredient : ingredient + ", "
                 })}</td>
-                <td>{sandwich.seasoning.map((seasoning, idx) => {
+                <td >{sandwich.seasoning.map((seasoning, idx) => {
                   return sandwich.seasoning.length - 1 === idx ? seasoning : seasoning + ", "
                 })}</td>
-                <td>{sandwich["meal-powers"].map((mealPower, idx) => {
+                <td >{sandwich["meal-powers"].map((mealPower, idx) => {
+                  return sandwich["meal-powers"].length - 1 === idx ? mealPower : mealPower + ", "
+                })}</td>
+              </tr>
+            })
+            }
+            {type && userMealPower && filteredSandwiches.length === 0 && <td colSpan={4} className="text-center"> <h4>Not sandwiches found</h4></td>}
+            {!type && !userMealPower && sandwiches.map((sandwich, index) => {
+              return <tr key={index}>
+                <td className='px-4'>{sandwich.name}</td>
+                <td className='px-4'>{sandwich.ingredient.map((ingredient, idx) => {
+                  return sandwich.ingredient.length - 1 === idx ? ingredient : ingredient + ", "
+                })}</td>
+                <td >{sandwich.seasoning.map((seasoning, idx) => {
+                  return sandwich.seasoning.length - 1 === idx ? seasoning : seasoning + ", "
+                })}</td>
+                <td >{sandwich["meal-powers"].map((mealPower, idx) => {
                   return sandwich["meal-powers"].length - 1 === idx ? mealPower : mealPower + ", "
                 })}</td>
               </tr>
             })}
-
           </tbody>
         </table>
       </div>
